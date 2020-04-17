28b3d1e584124860e94458515b1d55d505e7907e
AfonsoGR <afonso_rosa@hotmail.com>
2020-04-17 22:43:42 +0100

    Add over the shoulder view and partially functional switching

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float        _rotationSpeed;
-    public Transform    CameraRig, Player;
-    private float       _mouseX, _mouseY;
-    private float       _minDistance;    
-    private float       _maxDistance;   
-    private float       _smooth;   
-    private Vector3     _dollyDir;
-    private Vector3     _dollyDirAdjusted;
-    private float       _range; 
+    [SerializeField] private Camera         _mainCamera;
+    [SerializeField] private Camera         _leftShoulderCamera;
+    [SerializeField] private Camera         _rightShoulderCamera;
+    public float                            _rotationSpeed;
+    public Transform                        CameraRig, Player;
+    private float                           _mouseX, _mouseY;
+    private float                           _minDistance;    
+    private float                           _maxDistance;   
+    private float                           _smooth;   
+    private Vector3                         _dollyDir;
+    private Vector3                         _dollyDirAdjusted;
+    private float                           _range; 
 
     void Start()
     {
-        _rotationSpeed      = 1.0f;
-        _minDistance        = 1.0f; 
-        _maxDistance        = 4.5f; 
-        _smooth             = 10.0f;
-        _dollyDir           = transform.localPosition.normalized; 
-        _range              = transform.localPosition.magnitude; 
+        _rotationSpeed                  = 1.0f;
+        _minDistance                    = 1.0f; 
+        _maxDistance                    = 4.5f; 
+        _smooth                         = 10.0f;
+        _dollyDir                       = transform.localPosition.normalized; 
+        _range                          = transform.localPosition.magnitude;
+        _mainCamera.enabled             = true; 
+        _leftShoulderCamera.enabled     = false; 
+        _rightShoulderCamera.enabled    = false; 
     }
 
     void Update() 
@@ -39,6 +45,35 @@ public class CameraController : MonoBehaviour
         transform.LookAt(CameraRig);
 
         CameraRig.rotation = Quaternion.Euler(_mouseY, _mouseX, 0);
+
+        if (_mainCamera.enabled && Input.GetMouseButton(1))
+        {
+            _rightShoulderCamera.enabled = true;
+            Player.rotation = Quaternion.Euler(0, _mouseX, 0);
+
+            if (_rightShoulderCamera.enabled 
+                && Input.GetKeyDown(KeyCode.LeftAlt) 
+                && Input.GetMouseButton(1))
+            {
+                _leftShoulderCamera.enabled = true;
+                Player.rotation = Quaternion.Euler(0, _mouseX, 0);
+            }
+
+            if (_leftShoulderCamera.enabled 
+                && _rightShoulderCamera.enabled 
+                && Input.GetKeyDown(KeyCode.LeftAlt) 
+                && Input.GetMouseButton(1))
+            {
+                _rightShoulderCamera.enabled = true;
+                Player.rotation = Quaternion.Euler(0, _mouseX, 0);
+            }
+        }
+        else
+        {
+            _mainCamera.enabled             = true;
+            _leftShoulderCamera.enabled     = false; 
+            _rightShoulderCamera.enabled    = false; 
+        }
     }
 
     private void CamCollision()
