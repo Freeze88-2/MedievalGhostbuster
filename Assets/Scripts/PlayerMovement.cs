40a96c3f4ea79faff7e1544e6d0d77d249f0ab83
AfonsoGR <afonso_rosa@hotmail.com>
2020-04-17 11:54:49 +0100

    Add Movement Tweaks and Jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float _speed;
+    public float        _speed;
+    public float        _jumpHeight;
+    public bool         _isGrounded = true;
+    private Rigidbody   rb;
+
+    void Start() 
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
@@ -16,9 +24,26 @@ public class PlayerMovement : MonoBehaviour
         float _horizontalMov = Input.GetAxis("Horizontal");
         float _verticalMov = Input.GetAxis("Vertical");
 
-        Vector3 playerMov = 
-            new Vector3(_horizontalMov, 0f, _verticalMov).normalized
-            * _speed * Time.deltaTime;
-        transform.Translate(playerMov, Space.Self);
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            Vector3 playerMov = 
+                new Vector3(_horizontalMov, 0f, _verticalMov).normalized
+                * _speed * Time.deltaTime;
+            transform.Translate(playerMov, Space.Self);
+        }
+        
+        if (Input.GetButtonDown("Jump") && _isGrounded == true)
+        {
+            rb.AddForce(new Vector3(0, _jumpHeight, 0), ForceMode.Impulse);
+            _isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) 
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            _isGrounded = true;
+        }
     }
 }
