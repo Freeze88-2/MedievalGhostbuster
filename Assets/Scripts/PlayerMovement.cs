e55d893c04f9ec11178017d23e0ff8ce3bc2100f
AfonsoGR <afonso_rosa@hotmail.com>
2020-04-17 11:03:04 +0100

    Add basic Player and Camera Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float _speed;
+
+    void Update()
+    {
+        PlayerMov();
+    }
+
+    void PlayerMov()
+    {
+        float _horizontalMov = Input.GetAxis("Horizontal");
+        float _verticalMov = Input.GetAxis("Vertical");
+
+        Vector3 playerMov = 
+            new Vector3(_horizontalMov, 0f, _verticalMov).normalized
+            * _speed * Time.deltaTime;
+        transform.Translate(playerMov, Space.Self);
+    }
+}
