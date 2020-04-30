232d1499045a20dc886fd0328ade67766bc074cb
StarryNight00 <cisa.matias@gmail.com>
2020-04-30 15:32:43 +0100

    Player is animating with movement. Running backwards not rotating. Created script to control animations.

---
+++ Assets/Scripts/AnimController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+    private Animator            _characterAnim;
+    private PlayerMovement      _player;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _characterAnim = GetComponentInChildren<Animator>();
+        _player = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        AnimChangeOnWalk();
+    }
+
+    private void AnimChangeOnWalk()
+    {
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            _characterAnim.Play("Male Idle");
+        }
+        else if (Input.GetAxis("Horizontal") >= 0
+                && Input.GetAxis("Horizontal") <= 0)
+        {
+            _characterAnim.Play("Male Sprint");
+        }
+        else if (Input.GetAxis("Vertical") >= 0
+                && Input.GetAxis("Vertical") <= 0)
+        {
+            _characterAnim.Play("Male Sprint");
+        }
+    }
+}
