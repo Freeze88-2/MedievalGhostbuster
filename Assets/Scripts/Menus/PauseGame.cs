9569ad35652b263fab87d2286695e08bd64e8331
StarryNight00 <cisa.matias@gmail.com>
2020-06-23 16:07:04 +0100

    PauseGame script started. Game pauses.

--- Assets/Scripts/Menus/PauseGame.cs
+++ Assets/Scripts/Menus/PauseGame.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PauseGame : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private int escCount;
+    [SerializeField] private int tabCount;
+    private Camera menuCam;
+
     void Start()
     {
-        
+        menuCam = GetComponentInChildren<Camera>();
+        menuCam.enabled = false;
+        escCount = 0;
+        tabCount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void Pause()
+    {
+        if (Input.GetButtonUp("Pause"))
+        {
+            escCount++;
+            if (escCount == 1)
+            {
+                menuCam.enabled = true;
+                Time.timeScale = 0;
+            }
+            else if (escCount == 2)
+            {
+                menuCam.enabled = false;
+                Time.timeScale = 1;
+                escCount = 0;
+            }
+        }
     }
 }
