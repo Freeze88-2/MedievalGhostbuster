49492d09c3a1162588b305d9b8fdfff3ee9261d1
StarryNight00 <cisa.matias@gmail.com>
2020-06-24 02:17:58 +0100

    PauseFunctions are finished and ready for final testing. Volume controls are functional.

--- Assets/Scripts/Menus/PauseFunctions.cs
+++ Assets/Scripts/Menus/PauseFunctions.cs
@@ -2,12 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class PauseFunctions : MonoBehaviour
 {
+    [SerializeField] private AudioMixer _mixer;
+    [SerializeField] private AudioListener _listener;
+
+    private bool mute = false;
+
     public void ReloadScene()
     {
         Scene scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
+    }
+
+    public void VolumeSlider(float volumeValue)
+    {
+        _mixer.SetFloat("SceneMasterExp", Mathf.Log10(volumeValue) * 20);
+    }
+
+    public void Mute()
+    {
+        mute = !mute;
+
+        if (mute == true)
+            _listener.enabled = false;
+        
+        else if (mute == false)
+            _listener.enabled = true;
+    }
+
+    public void GoMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitToDesk()
+    {
+        Application.Quit();
     }
 }
